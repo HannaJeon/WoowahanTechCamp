e7f19e9e1a02f065db788ef977e49201f16bc0ea
HannaJeon <jks7psj@gmail.com>
2017-07-13 16:29:16 +0900

    addressBook init 커밋

---
+++ 07.13/AddressBook/AddressBook/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AddressBook
+//
+//  Created by woowabrothers on 2017. 7. 13..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        tableView.delegate = self
+//        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Int()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+

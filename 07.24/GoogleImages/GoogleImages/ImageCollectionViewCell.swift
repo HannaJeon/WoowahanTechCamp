0f47fc5eb2478439f7db1c59298916207d9eab36
HannaJeon <jks7psj@gmail.com>
2017-07-24 15:26:49 +0900

    GCD 과제

---
+++ 07.24/GoogleImages/GoogleImages/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  GoogleImages
+//
+//  Created by woowabrothers on 2017. 7. 24..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var numberLabel: UILabel!
+    
+}
---
+++ 07.24/GoogleImages/GoogleImages/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  GoogleImages
+//
+//  Created by woowabrothers on 2017. 7. 24..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCell
+        
+        cell.numberLabel.text = String(indexPath.row+1)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 128, height: 50)
+    }
+    
+}
+

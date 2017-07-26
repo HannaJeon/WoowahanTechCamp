be1dbc0e6df88433752ac5e6bfa1cbc7e5b34a8f
HannaJeon <jks7psj@gmail.com>
2017-07-26 16:02:08 +0900

    JoinView 기본 틀 구현

---
+++ 07.26/ChickenCount/ChickenCount/JoinViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JoinViewController.swift
+//  ChickenCount
+//
+//  Created by woowabrothers on 2017. 7. 26..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class JoinViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
